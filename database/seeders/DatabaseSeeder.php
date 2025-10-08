838ea715daaa19362240c69dace201ad2798a52c
sudarmono <30992530+monoland@users.noreply.github.com>
2025-10-08 19:42:05 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(\ModuleSystem\Seeders\DatabaseSeeder::class);
-        // $this->call(\ModuleReference\Seeders\DatabaseSeeder::class);
-        // $this->call(\ModuleFoundation\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleSystem\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleReference\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleFoundation\Seeders\DatabaseSeeder::class);
         $this->call(\ModuleMyFoundation\Seeders\DatabaseSeeder::class);
         // $this->call(\ModuleTraining\Seeders\DatabaseSeeder::class);
         // $this->call(\ModuleMyTraining\Seeders\DatabaseSeeder::class);
